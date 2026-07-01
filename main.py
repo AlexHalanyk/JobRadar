5881baee37f4faacd8ed772d0894d05877c5651e
Olesandr Halanyk <oh4002@hw.ac.uk>
2026-07-01 12:20:49 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def is_relevant(job):
+    return job["price"] >= 200
+
+jobs = [
+    {"route": "London -> Manchester", "cargo": "furniture", "price": 450, "link": "..."},
+    {"route": "Leeds -> Bristol", "cargo": "boxes", "price": 120, "link": "..."},
+    {"route": "Glasgow -> London", "cargo": "piano", "price": 600, "link": "..."}
+]
+
+for job in jobs:
+    print(job["route"], job["price"])
+
+for job in jobs:
+    if is_relevant(job):
+        print("Підходить:", job["route"], job["price"])
+
